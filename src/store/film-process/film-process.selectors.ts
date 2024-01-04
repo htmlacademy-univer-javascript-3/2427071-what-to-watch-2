a0525d5a1d8d6b78c8b4a6a4586e6f79bbbede9a
Vasilii Nazarenko <371436@edu.itmo.ru>
2024-01-05 02:11:52 +0300

    module9-task1: create tests for film-process

--- src/store/film-process/film-process.selectors.ts
+++ src/store/film-process/film-process.selectors.ts
@@ -3,6 +3,6 @@ import {IFilmPromoInfo} from '../../types/film-types.ts';
 import {IReview} from '../../types/review-types.ts';
 import {NameSpace} from '../../constants/namespaces.ts';
 
-export const getFilm = (state: State): IFilmPromoInfo | null => state[NameSpace.Film].currentFilm;
-export const getIsLoadingFilm = (state: State): boolean => state[NameSpace.Film].isLoadingFilm;
-export const getReviews = (state: State): IReview[] => state[NameSpace.Film].reviews;
+export const getFilm = (state: Pick<State, NameSpace.Film>): IFilmPromoInfo | null => state[NameSpace.Film].currentFilm;
+export const getIsLoadingFilm = (state: Pick<State, NameSpace.Film>): boolean => state[NameSpace.Film].isLoadingFilm;
+export const getReviews = (state: Pick<State, NameSpace.Film>): IReview[] => state[NameSpace.Film].reviews;
